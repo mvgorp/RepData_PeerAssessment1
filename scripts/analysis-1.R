d660d29bd88027384df3350308fc94f027931c6c
Martijn van Gorp <mvgorp@gmail.com>
2016-04-09 13:06:54 +0200

    Temp commit

--- scripts/analysis-1.R
+++ scripts/analysis-1.R
@@ -10,7 +10,11 @@ hist(stepsperday, main = "Total number of steps each day", xlab = "steps")
 mean = mean(stepsperday)
 
 # Median
-median = median(stepsperday) 
+median = median(stepsperday)
+
+# Make PNG
+dev.copy(png, file = "hist1.png", width = 800, height = 600)
+dev.off()
 
 
 
